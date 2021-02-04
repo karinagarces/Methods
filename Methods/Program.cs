07db90d4fa2e7bcd75c5210070c65b3858b9c809
Karina Garces <kgarces8@gmail.com>
2021-02-03 19:47:57 -0500

    committing method

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -6,7 +6,19 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name.");
+
+            string name = Console.ReadLine();
+            Hello(name);
+
+            Console.ReadKey(true);
         }
+
+         static void Hello(string name)
+        {
+            Console.WriteLine($"Hello {name}!");
+            
+        }
+
     }
 }
